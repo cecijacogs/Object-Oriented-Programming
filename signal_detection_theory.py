7ccdb8084f1263de91561e3bfb0ae9886f226256
hjsevill <hjsevill@uci.edu>
2025-02-06 21:30:11 +0000

    I started the coding for the probject!

--- signal_detection_theory.py
+++ signal_detection_theory.py
@@ -1,1 +1,12 @@
-# Begin Code Here
+import unittest 
+import numpy as np 
+import scipy
+from scipy.stats import norm
+
+class SignalDetection: 
+    def __init__(self, hits, misses, falseAlarms, correctRejections): # code gotten from ChatGPT
+        self.hits = hits
+        self.misses = misses
+        self.falseAlarms = falseAlarms
+        self.correctRejections = correctRejections
+        
